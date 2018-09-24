49fb359273d5c978353f1a68bafe38eefae56a0f
Gonza Gluzman <dggluz@gmail.com>
2018-09-24 17:12:35 -0300

    chore(client/jest): updated jest config

--- client/jest.config.js
+++ client/jest.config.js
@@ -66,7 +66,7 @@ module.exports = {
 	// A set of global variables that need to be available in all test environments
 	globals: {
 		'ts-jest': {
-			'tsConfigFile': 'tsconfig.json'
+			'tsConfig': 'tsconfig.json'
 		}
 	},
 
@@ -172,7 +172,7 @@ module.exports = {
 
 	// A map from regular expressions to paths to transformers
 	transform: {
-		'^.+\\.(ts|tsx)$': 'ts-jest'
+		'^.+\\.tsx?$': 'ts-jest'
 	},
 
 	// An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
