df63dc72d170408990e995918a1196850cf42753
Gonza Gluzman <dggluz@gmail.com>
2018-09-24 02:11:28 -0300

    feat(client/observable.mixin): WIP

---
+++ client/src/observable.mixin.ts
@@ -0,0 +1,34 @@
+type Constructor<T> = new (...args: any[]) => T;
+
+class Observer <T> {
+	constructor (private _eventName: string, private _callback: (x: T) => void) {}
+
+	notify (event: string, value: T) {
+		if (this._eventName === event) {
+			this._callback(value);
+		}
+		return this;
+	}
+}
+
+// TODO: type observers types from a generic map to functions
+
+export const Observable = <T extends Constructor<{}>> (Base: T = Object as any) =>
+	class extends Base {
+		private _observers: Observer<any>[] = [];
+
+		constructor (...args: any[]) {
+			super(...args);
+		}
+
+		subscribe <T> (eventName: string, callback: (x: T) => void) {
+			this._observers.push(new Observer(eventName, callback));
+			return this;
+		}
+
+		protected _notifyObservers <T = any> (event: string, value: T = undefined as any) {
+			this._observers.forEach(anObserver => anObserver.notify(event, value));
+			return this;
+		}
+	}
+;
