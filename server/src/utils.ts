7daea4c1a2a77ee573372927ed7cb629ea54bafe
Gonza Gluzman <dggluz@gmail.com>
2018-09-26 02:57:57 -0300

    feat(server/check-body.middleware): added checkBodyMiddleware

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,5 +1,6 @@
 import { Task, UnknownError } from '@ts-task/task';
 import { ParmenidesError } from 'parmenides';
+import { Overwrite } from 'type-zoo';
 
 /**
  * Takes a function that returns a sync result or throws an error and calls it from a
@@ -55,4 +56,19 @@ export const tap = <T> (fn: (x: T) => any) =>
 		fn(x);
 		return x;
 	}
+;
+
+/**
+ * Overwrite takes two objects and returns a new one, that is like the first one,
+ * overwritten with the second one. Mantains prototype chain of the firs object (in the result).
+ * @param target original object
+ * @param source object with the properties to overwrite
+ * @returns "merged" object
+ */
+export const overwrite = <A, B> (target: A, source: B) =>
+	Object.assign(
+		Object.create(target.constructor.prototype),
+		target,
+		source
+	) as any as Overwrite<A, B>
 ;
