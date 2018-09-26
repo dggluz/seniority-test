6a79259da037696d70280858b9a713d614ebd5bc
Gonza Gluzman <dggluz@gmail.com>
2018-09-26 17:12:46 -0300

    feat(server/saveNewItemCtrl): validating description and image

--- server/src/controllers/save-new-item.controller.ts
+++ server/src/controllers/save-new-item.controller.ts
@@ -9,6 +9,41 @@ import { FsError, SyntaxJSONError, InvalidJSONError } from '../fs-utils';
 import { BadRequestError } from '../http-errors';
 import { saveImageAsStatic, FileExtensionError } from '../utils/save-image-as-static';
 import { insertOneDocument, MongoDocument, isMongoError } from '../mongo-utils';
+import { rejectIf } from '../utils/reject-if';
+import * as sizeOf from 'image-size';
+import { tapChain } from '../utils/tap-chain';
+
+class ImageSizeError extends Error {
+	ImageSizeError = 'ImageSizeError';
+
+	constructor (public originalError: Error) {
+		super(originalError.message);
+	}
+}
+
+const getImageSize = (imagePath: string) =>
+	new Task<{
+		height: number;
+		width: number;
+	}, ImageSizeError>((resolve, reject) => {
+		sizeOf(imagePath, (err, dimensions) => {
+			if (err) {
+				reject(new ImageSizeError(err));
+			}
+			else {
+				resolve(dimensions);
+			}
+		});
+	})
+;
+
+class InvalidImageDimensions extends Error {
+	InvalidImageDimensions = 'InvalidImageDimensions';
+
+	constructor (expectedHeight: number, expectedWith: number) {
+		super(`Wrong image dimensions. Expected image to be ${expectedWith}x${expectedHeight}`);
+	}
+}
 
 interface MongoItem extends MongoDocument {
 	description: string;
@@ -16,6 +51,18 @@ interface MongoItem extends MongoDocument {
 }
 
 const saveItem = insertOneDocument<MongoItem>('items');
+
+const validateImageSize = (imagePath: string) => {
+	const expectedHeight = 320;
+	const expectedWidth = 320;
+
+	return getImageSize(imagePath)
+		.chain(rejectIf(
+			dimensions =>
+				dimensions.height !== expectedHeight || dimensions.width !== expectedWidth,
+			new InvalidImageDimensions(expectedHeight, expectedWidth)
+		));
+};
 
 /**
  * Endpoint that just response with a dummy object. Useful for checking if the server is alive.
@@ -27,8 +74,15 @@ export const saveNewItemCtrl = createEndpoint(req =>
 			description: str
 		})))
 		.chain(checkFiles(['image']))
-		// TODO: validate image (size)
-		// TODO: validate description
+
+		// validate description
+		.chain(rejectIf(
+			req => !/.{1,300}/.test(req.body.description),
+			new BadRequestError(new Error('Description should be non-empty and shorter than 300 characters'))
+		))
+
+		// validate image (size)
+		.chain(tapChain(req => validateImageSize(req.files.image.path)))
 
 		.chain(req =>
 			// Save item to DB
@@ -42,21 +96,25 @@ export const saveNewItemCtrl = createEndpoint(req =>
 			ok: true,
 			item: item
 		}))
-	.catch(caseError(isInstanceOf(FileExtensionError), err => Task.reject(new BadRequestError(err))))
-	.catch(
-		caseError(
-			isInstanceOf(
-				FsError,
-				SyntaxJSONError,
-				InvalidJSONError,
-			),
-			err => asUnknownError(err)
+		.catch(caseError(
+			isInstanceOf(FileExtensionError, InvalidImageDimensions, ImageSizeError),
+			err => Task.reject(new BadRequestError(err))
+		))
+		.catch(
+			caseError(
+				isInstanceOf(
+					FsError,
+					SyntaxJSONError,
+					InvalidJSONError,
+				),
+				err => asUnknownError(err)
+			)
+		)
+		.catch(
+			caseError(
+				isMongoError,
+				err => asUnknownError(err)
+			)
 		)
 	)
-	.catch(
-		caseError(
-			isMongoError,
-			err => asUnknownError(err)
-		)
-	)
-);
+;
