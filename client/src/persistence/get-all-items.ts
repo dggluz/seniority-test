fecd175960d9f2d8fdaa1f5a153e9f36553f4d4d
Gonza Gluzman <dggluz@gmail.com>
2018-09-27 01:41:18 -0300

    feat(client/get-all-items): validating server response on getAllItems

--- client/src/persistence/get-all-items.ts
+++ client/src/persistence/get-all-items.ts
@@ -1,7 +1,20 @@
 import { xhrTask } from '../utils/xhr-task';
 import { apiDomain } from './api-domain';
+import { arrOf, strictObjOf, str } from 'parmenides';
+import { validateServerResponse } from './validate-server-response';
 
-export const getAllItems = () => xhrTask({
-	url: `${apiDomain}/items`,
-	method: 'GET'
-});
+const getAllItemsContract = arrOf(
+	strictObjOf({
+		_id: str,
+		description: str,
+		image: str
+	})
+);
+
+export const getAllItems = () =>
+	xhrTask({
+		url: `${apiDomain}/items`,
+		method: 'GET'
+	})
+		.chain(validateServerResponse(getAllItemsContract))
+;
--- client/src/persistence/persistence.ts
+++ client/src/persistence/persistence.ts
@@ -1,17 +1,19 @@
 import { itemsStore } from '../model/model';
 import { saveNewItem } from './save-new-item';
 import { getAllItems } from './get-all-items';
+import { noop } from '../utils/noop';
 
 export const initPersistence = () => {
 	getAllItems()
+		.map(itemsData => {
+			itemsStore
+				.init(itemsData)
+				.subscribe('new-item', saveNewItem);
+		})
 		.fork(
 			// TODO: improve error handling
 			console.error,
-			itemsData => {
-				itemsStore
-					.init(itemsData)
-					.subscribe('new-item', saveNewItem);
-			}
+			noop
 		)
 	;
 };
