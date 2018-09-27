1d7fbc2b08017cabf031957cce49b74306ca2da5
Gonza Gluzman <dggluz@gmail.com>
2018-09-27 02:54:55 -0300

    feat(server/get-items): returning URLs full path

--- server/src/controllers/get-items.controller.ts
+++ server/src/controllers/get-items.controller.ts
@@ -4,12 +4,24 @@ import { caseError, isInstanceOf } from '@ts-task/utils';
 import { isMongoError } from '../db/mongo-utils';
 import { asUnknownError } from '@ts-task/task/dist/lib/src/operators';
 import { FsError, SyntaxJSONError, InvalidJSONError } from '../fs-utils';
+import { Task } from '@ts-task/task';
+import { configs } from '../configs';
 
 /**
  * Endpoint that returns all existing items
  */
 export const getItemsCtrl = createEndpoint(_ =>
-	getAllItems()
+	Task
+		.all([
+			getAllItems(),
+			configs
+		])
+		.map(([items, configs]) =>
+			items.map(item => ({
+				...item,
+				image: `./${configs.static.namespace}/images/${item.image}`
+			}))
+		)
 		.catch(caseError(isMongoError, err => asUnknownError(err)))
 		.catch(caseError(
 			isInstanceOf(FsError, SyntaxJSONError, InvalidJSONError),
