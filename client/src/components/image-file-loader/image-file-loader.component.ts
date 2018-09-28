4ee7e13d11742810ccfe3d354507b2c4f35990db
Gonza Gluzman <dggluz@gmail.com>
2018-09-28 11:15:53 -0300

    refactor(client/file-loader): merged file-loader component with image-file-loader component

--- client/src/components/image-file-loader/image-file-loader.component.ts
+++ client/src/components/image-file-loader/image-file-loader.component.ts
@@ -1,39 +1,51 @@
 import { Component } from '../component';
-import { FileLoaderComponent } from '../file-loader/file-loader.component';
 import { readFileAsDataUrl } from '../../utils/read-file-as-data-url';
+import { FileButtonComponent } from '../file-button/file-button.component';
+import { FileDropAreaComponent } from '../file-drop-area/file-drop-area.component';
 
 require('./image-file-loader.component.less');
 
 export class ImageFileLoaderComponent extends Component {
-	private _fileLoader: FileLoaderComponent;
+	private _fileButton: FileButtonComponent;
+	private _fileDropArea: FileDropAreaComponent;
 
 	constructor () {
 		super(require('./image-file-loader.component.html'));
-		this._fileLoader = new FileLoaderComponent()
+
+		this._fileButton = new FileButtonComponent()
 			.configureAcceptedType('image/*')
-			.appendTo(this.$dom.find('.file-loader-wrapper'))
-			.onNewFile(file => {
-				readFileAsDataUrl(file)
-					.fork(_ => {
-						// TODO: handle error.
-					}, image => {
-						// TODO: extract to method
-						this.$dom.find('.image-preview').removeClass('d-none');
-						this.$dom.find('.image-preview img').attr('src', image);
-					})
-				;
-			})
-			;
+			.onNewFile(file => this._handleNewFile(file))
+			.appendTo(this.$dom.find('.file-button-wrapper'))
+		;
+		this._fileDropArea = new FileDropAreaComponent()
+			.configureAcceptedType('image/*')
+			.onNewFile(file => this._handleNewFile(file))
+			.appendTo(this.$dom.find('.file-drop-area-wrapper'))
+		;
 	}
 
 	getImageFile () {
-		return this._fileLoader.getFile();
+		return this._fileButton.getFile() || this._fileDropArea.getFile();
 	}
 
 	restart () {
-		this._fileLoader.restart();
+		this._fileButton.restart();
+		this._fileDropArea.restart();
 		this.$dom.find('.image-preview').addClass('d-none');
 		this.$dom.find('.image-preview img').attr('src', '');
+		return this;
+	}
+
+	private _handleNewFile (file: File) {
+		readFileAsDataUrl(file)
+			.fork(_ => {
+				// TODO: handle error.
+			}, image => {
+				// TODO: extract to method
+				this.$dom.find('.image-preview').removeClass('d-none');
+				this.$dom.find('.image-preview img').attr('src', image);
+			})
+		;
 		return this;
 	}
 }
